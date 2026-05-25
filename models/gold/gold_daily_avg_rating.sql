a4018aa3247aa2e361d171bca9c99d8a5fa8e9cf
Murali Sampath <muralisampath55566@gmail.com>
2026-05-25 16:58:01 +0000

    gold layer

---
+++ models/gold/gold_daily_avg_rating.sql
@@ -0,0 +1,15 @@
+{{ config(
+    schema='gold',
+    materialized='table',
+    alias ='dbt_gold_daily_avg_rating'
+) }}
+
+SELECT
+    date(date_format(r.created_at, 'yyyy-MM-dd')) as review_date,
+    r.product_id,
+    p.product_name,
+    avg(r.rating) as avg_rating
+FROM {{ ref('raw_reviews') }} r 
+LEFT JOIN {{ ref('silver_products') }} p 
+ON r.product_id = p.id
+group by all
